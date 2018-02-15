6991e98afa63dd5a6170a49a27aa2aa99e98287c
Lattimeria <lobankovaalyona@gmail.com>
2018-02-15 12:48:08 +0700

    Create game "Snake"

--- Sssnake/Form1.cs
+++ Sssnake/Form1.cs
@@ -15,6 +15,31 @@ namespace Sssnake
         public Form1()
         {
             InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog; // мышкой нельзя растягивать форму
+            this.MaximizeBox = false; // недоступна кнопка "развернуть во весь экран"
+            this.StartPosition = FormStartPosition.CenterScreen; // форма по центру экрана
+
+            Game game = new Game();
+            
         }
+        // классы: игра, змея, еда
+        // 
+        // еда появляется на поле, может быть съедена змеей, после исчезает
+        // змея двигается по полю
+        // игра следит за координатами, создает еду, отслеживает пересечения и решает что делать
+        // добавить длину змеи или закончить игру
+
+        /*Pen myPen = new Pen(Color.Red);
+        Graphics g;
+        g = this.CreateGraphics();
+        g.DrawRectangle(myPen, new Rectangle(0, 0, 30, 30));
+            myPen.Dispose();
+            g.Dispose();
+            */
+        
+            
+
+
     }
 }
---
+++ Sssnake/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sssnake
+{
+    class Game
+    {
+        List<Food> foodList = new List<Food>();
+        
+        void Start()
+        {
+            
+        } 
+
+        void Draw()
+        {
+
+        }
+        
+    }
+    class Coordinate
+    {
+        int X;
+        int Y;
+    }
+}
